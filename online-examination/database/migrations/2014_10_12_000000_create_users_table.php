5b246c628cff32771e4cc06ed4f55073984ee4ce
dathaohus <bombomqt98@gmail.com>
2021-01-14 16:05:02 +0700

    db migration

--- online-examination/database/migrations/2014_10_12_000000_create_users_table.php
+++ online-examination/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('mobile');
+            $table->datetime('lastlogin');
+            $table->bigInteger('role');
+            $table->string('provide');
+            $table->string('provide_id');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ online-examination/database/migrations/2021_01_14_075323_create_subject_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject_category', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_subject');
+            $table->string('subject_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject_category');
+    }
+}
